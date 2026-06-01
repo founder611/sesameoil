4237c21924993db17d678592e8a1897bb7947239
founder611 <founder@ecomonks.in>
2026-06-01 18:15:37 +0530

    updation1

--- sesameoil/settings.py
+++ sesameoil/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6-_rd#s&p5ycf$s2geaohjcj+!r0no)m8drbgy9r8kpj-%5juo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
